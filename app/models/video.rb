d345c5c388f014a78dc271f078abaec874808375
Marcelo Soares Souza <marcelo@juntadados.org>
2013-03-07 15:08:15 -0300

    * Improvement in Video * Removed JW Player * Added Video.JS (Free, Open, Libre...) * HTML5 In, Flash Out. :) * Default Video format now is OGV (OGG)

--- app/models/video.rb
+++ app/models/video.rb
@@ -7,7 +7,7 @@ class Video < ActiveRecord::Base
   has_attached_file :video,
                     :url => "/system/entidade_equipamentos/videos/:id/:filename",
                     :path => ":rails_root/public/system/entidade_equipamentos/videos/:id/:filename",
-                    :styles => { :medium => { :geometry => "640x480", :format => 'flv' },
+                    :styles => { :medium => { :geometry => "640x480", :format => 'ogv' },
                                  :thumb  => { :geometry => "300x300#", :format => 'jpg', :time => 10 }
                                }, :processors => [:ffmpeg]
 
