169c2f1999e8458b83f34fe114986ba503170255
Marcelo Soares Souza <marcelo@juntadados.org>
2013-03-21 11:43:11 -0300

    * Changed Audio Format in Video Conversion, now using libvorbis * Disable submit button when processing

--- app/models/video.rb
+++ app/models/video.rb
@@ -9,7 +9,9 @@ class Video < ActiveRecord::Base
   has_attached_file :video,
                     :url => "/system/entidade_equipamentos/videos/:id/:filename",
                     :path => ":rails_root/public/system/entidade_equipamentos/videos/:id/:filename",
-                    :styles => { :medium => { :geometry => "640x480", :format => 'ogv' },
+                    :styles => { :medium => { :geometry => "640x480", :format => 'ogv', 
+                                              :convert_options => { :output => { :acodec => 'libvorbis', :ab => '128k', :ac => 2 } } 
+                                            },
                                  :thumb  => { :geometry => "300x300#", :format => 'jpg', :time => 10 }
                                }, :processors => [:ffmpeg]
 
