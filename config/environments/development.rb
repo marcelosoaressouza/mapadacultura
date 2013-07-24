8383235aa43a5906a73d8b14e7df6802c7e784ba
Marcelo Soares Souza <marcelo@juntadados.org>
2013-07-24 08:58:34 -0300

    * Initial mailer support. Send notification when a Entidade or Equipamento are created and published.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,18 @@
 Mapadacultura::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "localhost",
+    port: 25,
+    # domain: "mapadacultura.org",
+    # authentication: "plain",
+    # user_name: "user",
+    # password: "password",
+    # enable_starttls_auto: true
+  }
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,18 @@
 Mapadacultura::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "localhost",
+    port: 25,
+    # domain: "mapadacultura.org",
+    # authentication: "plain",
+    # user_name: "user",
+    # password: "password",
+    # enable_starttls_auto: true
+  }
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
